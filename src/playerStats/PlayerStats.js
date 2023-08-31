488c81435bd5b7b8417f2b79455c52782bd36d98
Deyvison Queiroz <deyvisondev.geral@gmail.com>
2023-08-31 14:24:55 +0100

    "Name and Background"

---
+++ src/playerStats/PlayerStats.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/playerStats.css"
+
+const PlayerStats = () => {
+    return (
+        <div id="playerStats">
+            <h1>DEYVISON QUEIROZ</h1>
+            <h1>LEVEL 36</h1>
+            <div id="playerStats__lines">
+                <span id="playerStats__lines__thick"></span>
+                <span id="playerStats__lines__thin"></span>
+            </div>
+            <h2>Fullstack Developer</h2>
+        </div>
+    )
+};
+
+export default PlayerStats;
